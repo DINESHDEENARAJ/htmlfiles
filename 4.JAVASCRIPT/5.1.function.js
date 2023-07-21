d4c6b8fa48856bfdd5726d188955dfedd1cc9b1c
DINESHDEENARAJ <grdeena96@gmail.com>
2023-07-21 20:16:18 +0530

    htmlfiles

---
+++ 4.JAVASCRIPT/5.1.function.js
@@ -0,0 +1,27 @@
+function data()
+{
+    alert("this is alert")
+}
+
+function data1(studentname,clgname)
+{
+    alert("student name is :"+studentname+"college name is:"+clgname)
+}
+
+// function data1(studentname,clgname)
+// {
+//     document.writeln("student name is :"+studentname+"college name is:"+clgname)
+// }
+
+
+var exe=()=>
+{
+    document.writeln("this is arrow functioin")
+}
+
+const sum=(a,b)=>
+{
+    return a+b;
+}
+
+
